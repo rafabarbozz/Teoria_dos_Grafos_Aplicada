bab26df07b06b3303eaa94d54bf44d3d9830dcea
Rafael Almeida <rafael.almeida1096@gmail.com>
2024-04-03 16:00:56 -0300

    Algoritmo karger e gráfico do karger

--- Trabalho3/algoritmos.py
+++ Trabalho3/algoritmos.py
@@ -1,6 +1,6 @@
-from random import seed, sample
+from random import sample, seed
         
-class Krager:
+class Karger:
     def __init__(self, lista_adj: list, lista_aresta: list):
         ''''
         Lista_adj(list): é a lista de adjacência do grafo
@@ -11,43 +11,40 @@ class Krager:
 
     
     def run(self):
-        seed(1)
-        lista_aresta_copy = self.lista_aresta.copy()
-        lista_adj_copy = self.lista_adj.copy()
-        
-        while lista_adj_copy.count([]) < (len(lista_adj_copy) - 2): # Se só restarem 2 vértices não vazios finaliza o loop
+        lista_aresta_copy = self.lista_aresta.copy() # criando uma cópia
+        lista_adj_copy = [adj_lista.copy() for adj_lista in self.lista_adj]  # Criando uma cópia profunda da lista de adjacência
+
+        while len([lst for lst in lista_adj_copy if lst]) > 2: # Se só restarem 2 vértices não vazios finaliza o loop
             aresta_escolhida = sample(lista_aresta_copy, 1)[0] # Escolhendo uma aresta aleatoriamente
-            
-            print(f'Aresta escolhida: {aresta_escolhida}')  
-            print(f'Lista_adj antes: {lista_adj_copy}') 
-            print(f'Lista_aresta antes: {lista_aresta_copy}\n')
-            
-            
+
             for elemento in (lista_adj_copy[aresta_escolhida[1] - 1]):
-                # Adicionando na lista de adjacencia do primeiro elemento todos os vertices do segundo elemento
+                # Adicionando na lista de adjacencia do primeiro elemento todos os vertices do segundo elemento tirando ele mesmo
                 if elemento not in lista_adj_copy[aresta_escolhida[0] - 1] and elemento != aresta_escolhida[0]:
                     lista_adj_copy[aresta_escolhida[0] - 1].append(elemento)
                 
-                # Adicionando a lista de aresta todas as arestas do segundo elemento para o primeiro
+                # Adicionando na lista de aresta todas as arestas do segundo elemento para o primeiro
                 if (aresta_escolhida[0], elemento) not in lista_aresta_copy and (elemento, aresta_escolhida[0]) and elemento != aresta_escolhida[0]:
                     lista_aresta_copy.append((aresta_escolhida[0], elemento))
             
-             
-            # Loop usado para retirar as arestas que o segundo elemento da aresta escolhida possui na lista de arestas
-            for aresta in lista_aresta_copy:
-                if aresta[0] == aresta_escolhida[1]:
-                    lista_aresta_copy.remove(aresta)
-                    
-                elif aresta[1] == aresta_escolhida[1]:
-                    lista_aresta_copy.remove(aresta)
-                
-                
-            # Esvaziando lista adj na segunda posição da aresta escolhida
-            lista_adj_copy[aresta_escolhida[1] - 1].clear()
             
-            
-            print(f'Lista_adj depois: {lista_adj_copy}') 
-            print(f'Lista_aresta depois: {lista_aresta_copy}')
-            print('\n')
+            # Loop usado para retirar as arestas que o segundo elemento da aresta escolhida possui na lista de arestas, se usar o remove pode dar problema com 
+            # elementos sendo pulados
+            lista_aresta_copy = [aresta for aresta in lista_aresta_copy if aresta_escolhida[1] not in aresta]
     
+                 
+            # Esvaziando lista adj na posição do segundo elemento da aresta escolhida
+            lista_adj_copy[aresta_escolhida[1] - 1].clear()
+        
         return len(lista_aresta_copy)
+    
+
+    def executar_n(self, n):
+        corte_minimo = float('inf')
+        for _ in range(n):
+            qtd_corte, lista_adj = (self.run())
+            if qtd_corte < corte_minimo:
+                corte_minimo = qtd_corte
+        
+        return corte_minimo
+    
+    
--- Trabalho3/main.py
+++
@@ -1,8 +0,0 @@
-from func_ler_grafo import ler_grafo
-from algoritmos import Krager
-
-
-lista_adj, lista_aresta = ler_grafo('Trabalho3/in/graph_1')
-
-algoritmo = Krager(lista_adj, lista_aresta)
-print(algoritmo.run())
